e1caef0f9ec1943f11bb2ea4b195cc876165a64e
Cyb3r R4ider <ant.thodos@gmail.com>
2023-09-14 14:29:22 +0300

    Constant Class in Controllers

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,8 +4,11 @@ declare (strict_types=1);
 require __DIR__."/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
+
+echo HomeController::class;
 
 $app=new App();
-$app->get('/',['App\Controllers\HomeController','home']);
+$app->get('/',[HomeController::class,'home']);
 dd($app);
 return $app;
